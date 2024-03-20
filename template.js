300bf6ffa37b3c00353c24cdc133c0f71bb26b11
Chidera Umejuru <deraumejuru@gmail.com>
2024-03-20 02:24:33 +0100

    added template literal

---
+++ template.js
@@ -0,0 +1,15 @@
+// template literal allows us use backtiks to make shorter codes
+let firstName = `Chidera`
+let lastName = `Umejuru`
+const fullName = `${firstName} ${lastName}`
+console.log(fullName);
+
+// makes multi-line easier
+let myName = `Chidera Umejuru`
+let age = 20
+let gender = `female`
+const info = `${myName}
+${age}
+${gender}
+`
+console.log(info);
