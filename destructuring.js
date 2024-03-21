87993709e0609b1876fc9de2c8245c5e8db18b44
Chidera Umejuru <deraumejuru@gmail.com>
2024-03-21 15:42:42 +0100

    modified destructing.js

--- destructuring.js
+++ destructuring.js
@@ -11,6 +11,16 @@ const {Age,City} = personalInfo
 console.log(`${Age}
 ${City}`);
 
+let details = {
+    "id": 1,
+    "first_name": "Johnath",
+    "last_name": "Bounde",
+    "email": "jbounde0@github.io",
+    "gender": "Female"
+}
+const {gender, id} = details
+console.log(`${gender} ${id}`);
+
 // destructuring array
 const [Country, state, localgovt] = ["Nigeria","RiversState","Onelga"]
 
